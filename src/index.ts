afcf58745efcd6129a470822e341c0833162a765
keifufu <keifufu@noonly.net>
2023-03-06 14:52:03 +0100

    cleaning

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,12 @@ import express from 'express'
 import { z } from 'zod'
 
 dotenv.config()
-const hook = new Webhook(process.env.WEBHOOK_URL || '')
+
+if (!process.env.PORT) throw new Error('PORT is not defined')
+if (!process.env.WEBHOOK_URL) throw new Error('WEBHOOK_URL is not defined')
+if (!process.env.WEBHOOK_AVATAR_URL) throw new Error('WEBHOOK_AVATAR_URL is not defined')
+
+const hook = new Webhook(process.env.WEBHOOK_URL)
 const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -15,8 +20,7 @@ const ReportBodySchema = z.object({
   extVersion: z.string(),
 })
 
-const cache: Record<string, string> = {}
-
+const cache: Record<string, boolean> = {}
 app.post('/report', (req, res) => {
   try {
     const body = ReportBodySchema.parse(req.body)
@@ -24,11 +28,11 @@ app.post('/report', (req, res) => {
     if (body.type === 'automatic') {
       const msg = `${body.extVersion} - ${body.message}`
       if (cache[msg]) return res.status(200).send('OK')
-      cache[msg] = msg
+      cache[msg] = true
     }
 
     hook.setUsername('WNPRedux Reporter')
-    hook.setAvatar(process.env.WEBHOOK_AVATAR_URl || '')
+    hook.setAvatar(process.env.WEBHOOK_AVATAR_URL as string)
     hook.send(`**Type:** ${body.type}\n**Message:** ${body.message}\n**Version:** ${body.extVersion}`)
     res.status(200).send('OK')
   } catch (e) {
